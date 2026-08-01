ed157fdba60c16264566e4bbf1029a61960d8af4
mst-mkt <mail@keito.dev>
2026-08-02 04:07:31 +0900

    init: nur repository

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  outputs =
+    inputs:
+    let
+      forAllSystems = inputs.nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+    in
+    {
+      packages = forAllSystems (
+        system:
+        import ./. {
+          pkgs = inputs.nixpkgs.legacyPackages.${system};
+        }
+      );
+
+      overlays.default = final: _prev: import ./. { pkgs = final; };
+
+      formatter = forAllSystems (system: inputs.nixpkgs.legacyPackages.${system}.nixfmt-tree);
+    };
+
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+  };
+}
