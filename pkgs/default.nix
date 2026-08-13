dd59185e29d931f827fcae629e51d6a0d92a11da
mst-mkt <mail@keito.dev>
2026-08-14 01:33:46 +0900

    pkgs: esaio/esa-cli

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,6 +1,7 @@
 { pkgs }:
 {
   calldiff = pkgs.callPackage ./calldiff { };
+  esa-cli = pkgs.callPackage ./esa-cli { };
   gengo = pkgs.callPackage ./gengo { };
   gh-pr-reviews = pkgs.callPackage ./gh-pr-reviews { };
   git-hunk = pkgs.callPackage ./git-hunk { };
